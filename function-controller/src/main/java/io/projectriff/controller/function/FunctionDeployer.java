a2ffd7584ddc9f26dfd3d694528ad81adea38271
Eric Bottard <eric.bottard+github@gmail.com>
2017-11-23 15:19:41 +0100

    Use --key=value to pass args to sidecar
    Fixes #4

--- function-controller/src/main/java/io/projectriff/controller/function/FunctionDeployer.java
+++ function-controller/src/main/java/io/projectriff/controller/function/FunctionDeployer.java
@@ -16,37 +16,33 @@
 
 package io.projectriff.controller.function;
 
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.ContainerBuilder;
 import io.fabric8.kubernetes.api.model.EmptyDirVolumeSourceBuilder;
-import io.fabric8.kubernetes.api.model.EnvVar;
-import io.fabric8.kubernetes.api.model.EnvVarBuilder;
 import io.fabric8.kubernetes.api.model.PodSpec;
 import io.fabric8.kubernetes.api.model.PodSpecBuilder;
 import io.fabric8.kubernetes.api.model.VolumeBuilder;
 import io.fabric8.kubernetes.api.model.VolumeMount;
 import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
-
 import io.projectriff.kubernetes.api.model.XFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 /**
  * Makes sure a certain function is running on Kubernetes.
  *
- * <p>This implementation uses k8s deployments with a desired replicas count (possibly 0).</p>
+ * <p>
+ * This implementation uses k8s deployments with a desired replicas count (possibly 0).
+ * </p>
  *
  * @author Eric Bottard
  * @author Mark Fisher
@@ -132,7 +128,7 @@ public class FunctionDeployer {
 		ContainerBuilder builder = new ContainerBuilder().withName("sidecar")
 				.withImage(SIDECAR_IMAGE + ":" + sidecarProperties.getTag())
 				.withImagePullPolicy("IfNotPresent")
-				.withEnv(buildSidecarEnvVars(function));
+				.withArgs(buildSidecarArgs(function));
 		if ("stdio".equals(function.getSpec().getProtocol())) {
 			builder.withVolumeMounts(buildNamedPipesMount());
 		}
@@ -143,34 +139,16 @@ public class FunctionDeployer {
 		return new VolumeMountBuilder().withMountPath("/pipes").withName("pipes").build();
 	}
 
-	private EnvVar[] buildSidecarEnvVars(XFunction function) {
+	private List<String> buildSidecarArgs(XFunction function) {
 		String outputDestination = function.getSpec().getOutput();
 		if (!StringUtils.hasText(outputDestination)) {
 			outputDestination = "replies";
 		}
-		Map<String, Object> config = new LinkedHashMap<>();
-		config.put("spring.cloud.stream.bindings.input.destination", function.getSpec().getInput());
-		config.put("spring.cloud.stream.bindings.output.destination", outputDestination);
-		config.put("spring.cloud.stream.bindings.input.group", function.getMetadata().getName());
-		config.put("spring.profiles.active", function.getSpec().getProtocol());
-		config.put("spring.application.name", "sidecar-" + function.getSpec().getInput());
-
-		config.put("spring.cloud.stream.kafka.binder.brokers", System.getenv("SPRING_CLOUD_STREAM_KAFKA_BINDER_BROKERS"));
-		config.put("spring.cloud.stream.kafka.binder.zkNodes", System.getenv("SPRING_CLOUD_STREAM_KAFKA_BINDER_ZK_NODES"));
-		config.put("server.port", "-1");
-
-		String json;
-		try {
-			json = objectMapper.writeValueAsString(config);
-		}
-		catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-
-		return new EnvVar[] {
-				new EnvVarBuilder().withName("JAVA_TOOL_OPTIONS").withValue("-Xmx512m").build(),
-				new EnvVarBuilder().withName("SPRING_APPLICATION_JSON").withValue(json).build()
-
-		};
+		return Arrays.asList(
+				"--inputs", function.getSpec().getInput(),
+				"--outputs", outputDestination,
+				"--group", function.getMetadata().getName(),
+				"--protocol", function.getSpec().getProtocol(),
+				"--brokers", System.getenv("SPRING_CLOUD_STREAM_KAFKA_BINDER_BROKERS"));
 	}
 }
